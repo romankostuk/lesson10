0a5247da359c8243bdbe50609f39b793424561f6
romankostuk <moryak.man@list.ru>
2024-06-11 00:01:55 +0300

    Add files via upload

---
+++ lesso10.py
@@ -0,0 +1,54 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while True:
+    my_list[0] = 42
+    my_list[1] = 69
+    my_list[2] = 322
+    my_list[3] = 13
+    my_list[4] = 0
+    my_list[5] = 99
+    my_list[6] = -5
+    my_list[7] = 9
+    my_list[8] = 80
+    my_list[9] = 7
+    my_list[10] = -6
+    my_list[11] = 5
+    number = int(input('Введите число из списка: '))
+    if number >= 0 and number == my_list[0]:
+        print('Отличный результат')
+        continue
+    if number >= 0 and number == my_list[1]:
+        print('Отличный результат')
+        continue
+    if number >= 0 and number == my_list[2]:
+        print('Отличный результат')
+        continue
+    if number >= 0 and number == my_list[3]:
+        print('Отличный результат')
+        continue
+    if number >= 0 and number == my_list[4]:
+        print('Отличный результат')
+    if number >= 0 and number == my_list[5]:
+        print('Отличный результат')
+        continue
+    if number >= 0 and number == my_list[6]:
+        print('Отличный результат')
+        continue
+    if number >= 0 and number == my_list[7]:
+        print('Отличный результат')
+        continue
+    if number >= 0 and number == my_list[8]:
+        print('Отличный результат')
+        continue
+    if number >= 0 and number == my_list[9]:
+        print('Отличный результат')
+        continue
+    if number >= 0 and number == my_list[10]:
+        print('Отличный результат')
+        continue
+    if number >= 0 and number == my_list[11]:
+        print('Отличный результат')
+    else:
+        break
+
+
+
